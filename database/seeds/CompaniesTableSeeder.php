7c9018d23d942b8a6230e836a4a5995f983435cf
AlexandrFrog599 <frogsasa599@gmail.com>
2021-11-15 13:22:41 +0200

    create  seederes

--- database/seeds/CompaniesTableSeeder.php
+++ database/seeds/CompaniesTableSeeder.php
@@ -11,6 +11,16 @@ class CompaniesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        for ($i=0; $i < 10; $i++) {
+            $cName = 'Компания '.$i;
+            $companies[] = [
+                'name' => $cName,
+                'email' => Str::random(10).'@gmail.com',
+                'phone' => Str::random(10),
+                'website' => Str::random(10),
+                // 'logo' => Str::random(10).'.jpg',
+            ];
+        }
+    \DB::table('companies')->insert($companies);
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
-use Illuminate\Database\Seeder;
+ use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+use Faker\Factory as Faker;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +15,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-    }
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@local.in',
+            'password' => bcrypt('password'),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        $this->call(CompaniesTableSeeder::class);
+
+		$faker = Faker::create('ru_RU');
+        for ($i=0; $i < 100 ; $i++) {
+            $employees[] = [
+	            'company_id' => rand(1,10),
+	            'first_name' => $faker->firstName,
+	            'last_name' => $faker->lastName,
+	            'email' => $faker->email,
+	            'phone' => $faker->phoneNumber,            	
+            ];
+        }
+        DB::table('employees')->insert($employees);
+   	}
 }
